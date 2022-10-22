d3e5e96a770b13fa27f8a38f42fcf78dda144848
a.matchin <a.matchin@prequelapp.com>
2022-10-22 14:02:32 +0300

    Third commit on master

--- app/src/main/java/ru/matchin/gittest2210/MainActivity.java
+++ app/src/main/java/ru/matchin/gittest2210/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hello and goodbye world");
+
+        // самые новые изменения
     }
 }
